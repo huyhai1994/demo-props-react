17a38820e3143ade1df317c8856d9b799881ce07
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-17 15:18:04 +0700

    update project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Welcome from './components/Welcome';
 
+/*TODO: Bước 4: Sử dụng component Welcome trong Component App*/
 function App() {
     return (<div className="App">
         <Welcome name="Admin"/>
